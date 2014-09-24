b9bde81f5fb32a7fb5f091e3799107112e845c37
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-09-24 16:38:26 +0100

    Update to the latest mirage-tcpip changes

--- proxy/proxy.ml
+++ proxy/proxy.ml
@@ -6,9 +6,6 @@ module Main (C: CONSOLE) (S: STACKV4) = struct
 
   module I = Init.Main(C)(S)
 
-  let () =
-    Arpv4.enable_promiscuous_mode ()
-
   let start c s =
     Tcpv4.Pcb.set_mode `Fast_start_proxy;
     I.start c s >>= fun _ip ->
